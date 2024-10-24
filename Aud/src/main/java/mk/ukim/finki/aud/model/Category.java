88b661f9d8cfe7e9b28ad3f70339ab4bf65a7c9d
Elena Kolevska <kolevska_elena@yahoo.com>
2024-10-24 15:29:10 +0200

    Added directory

---
+++ Aud/src/main/java/mk/ukim/finki/aud/model/Category.java
@@ -0,0 +1,34 @@
+package mk.ukim.finki.aud.model;
+
+import lombok.Data;
+
+@Data
+public class Category {
+        private String name;
+        private String description;
+
+
+        public Category(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+
+}
